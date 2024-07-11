5fe5f5c9c72432dd9f47f641b826f4975562c51d
Giancarlo Tutini <tutini.giancarlo@gmail.com>
2024-07-11 00:30:49 -0300

    feat: new text on home screen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,8 +11,8 @@ export default function SplashScreen() {
     <div className="flex h-screen items-center justify-center">
       <Logo />
       <div className="absolute inset-x-0 bottom-4 text-center">
-        <h1 className="text-4xl">BrainBox</h1>
-        <p className="text-base">Version 1.0</p>
+        <h1 className="text-4xl poppins-500">FintechX</h1>
+        <p className="text-base poppins-300">Version 1.0</p>
       </div>
     </div>
   );
